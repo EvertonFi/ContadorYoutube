0cdb8e79f9915eb69a5b161ea6057e787b1c7288
EvertonFi <jose.everton.f@hotmail.com>
2020-03-09 21:23:30 -0300

    First Commint

---
+++ android/app/src/main/kotlin/com/example/contadoryoutube/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.contadoryoutube
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _input = GlobalKey<FormState>();
+
+  String nome;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        centerTitle: true,
+        title: Text('Contador de Inscritos'),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        color: Colors.blue[50],
+        child: ListView(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Padding(
+                    padding: const EdgeInsets.all(30),
+                    child: Form(
+                      key: _input,
+                      child: TextFormField(
+                        style: TextStyle(fontSize: 30),
+                        onSaved: (input) => nome = input,
+                      ),
+                    ),
+                  ),
+                ),
+                CircleAvatar(
+                  radius: 30,
+                  child: IconButton(
+                    iconSize: 40,
+                    color: Colors.white,
+                    icon: Icon(Icons.keyboard_arrow_right),
+                    onPressed: () {
+                      if (_input.currentState.validate()) {
+                        _input.currentState.save();
+                        print(nome.toString());
+                      }
+                    },
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: CircleAvatar(
+                radius: 100,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Center(
+                child: Text(
+                  '0',
+                  style: TextStyle(fontSize: 50),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
