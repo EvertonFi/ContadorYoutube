128be3c86c6d8e15f96369bd62eb4db86e4f3bab
EvertonFi <jose.everton.f@hotmail.com>
2020-03-21 14:28:32 -0300

    Update

--- lib/const.dart
+++
@@ -1,3 +0,0 @@
-class ConstAPP {
-  static String nomeCanal = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=EvenusFi&type=channel&maxResults=1&key=AIzaSyDjS5xa8lRwf7AlDa48JvVJbpTR8mcgQJI';
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,132 @@
-import 'package:contadoryoutube/ApiCanal.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 void main() {
-  
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-
-
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: MyHomePage(),
     );
   }
 }
 
-class Home extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _HomeState extends State<Home> {
+class _MyHomePageState extends State<MyHomePage> {
+  String inscritos = ' ';
+  String urlCanal = " ";
+  final controller = TextEditingController();
 
-  
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
-  final _input = GlobalKey<FormState>();
+  getInfo(String canal) async {
+    if (canal != "") {
+      var url =
+          "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$canal&type=channel&maxResults=1&key=AIzaSyDjS5xa8lRwf7AlDa48JvVJbpTR8mcgQJI";
 
-  String nome;
+      var response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        var jsonResponse = convert.jsonDecode(response.body);
+        var channelId = jsonResponse['items'][0]['id']['channelId'];
+        var url = jsonResponse['items'][0]['snippet']['thumbnails']['medium']['url'];
+        setState(() {
+          urlCanal = url;
+        });
+        return await getCont(channelId);
+      }
+    }else{
+      setState(() {
+        inscritos = "Error";
+      });
+    }
+  }
+
+  getCont(String id) async {
+    var url =
+        "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=$id&key=AIzaSyDjS5xa8lRwf7AlDa48JvVJbpTR8mcgQJI";
+
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body);
+      var subscriberCount =
+          jsonResponse['items'][0]['statistics']['subscriberCount'];
+      setState(() {
+        inscritos = subscriberCount;
+        print(inscritos);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.red,
       appBar: AppBar(
         backgroundColor: Colors.red,
         centerTitle: true,
-        title: Text('Contador de Inscritos', style: TextStyle(fontSize: 30),),
+        title: Text(
+          'Contador de Inscritos',
+          style: TextStyle(fontSize: 30),
+        ),
       ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        color: Colors.blue[50],
-        child: ListView(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: Padding(
-                    padding: const EdgeInsets.all(30),
-                    child: Form(
-                      key: _input,
-                      child: TextFormField(
-                        style: TextStyle(fontSize: 30),
-                        onSaved: (input) => nome = input,
-                      ),
-                    ),
-                  ),
+      body: Wrap(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 10, left: 20, right: 20, bottom: 10),
+                child: TextField(
+                  controller: controller,
+                  style: TextStyle(fontSize: 40),
                 ),
-                CircleAvatar(
-                  radius: 30,
-                  child: IconButton(
-                    iconSize: 40,
-                    color: Colors.white,
-                    icon: Icon(Icons.keyboard_arrow_right),
-                    onPressed: () {
-                      if (_input.currentState.validate()) {
-                        _input.currentState.save();
-                        print(nome.toString());
-                      }
-                    },
-                  ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              child: CircleAvatar(
-                radius: 100,
-                backgroundColor: Colors.blue[200],
-                child: Image.asset('assets/img/Icone.png'),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              child: Center(
+              CircleAvatar(
+                radius: 30,
+                child: IconButton(
+                    icon: Icon(Icons.arrow_forward),
+                    onPressed: () {
+                      getInfo(controller.text);
+                    }),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: urlCanal != " " ? NetworkImage(urlCanal) : NetworkImage('https://imageog.flaticon.com/icons/png/512/174/174883.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF'),
+              ),
+              Padding(
+                padding:
+                    EdgeInsets.only(top: 30, right: 50, left: 50, bottom: 20),
                 child: Text(
-                  '329',
+                  inscritos == ' ' ? ' ' : inscritos,
                   style: TextStyle(fontSize: 50),
                 ),
-              ),
-            )
-          ],
-        ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
